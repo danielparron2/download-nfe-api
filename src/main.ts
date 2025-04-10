6b52210fc5f2edc34aacf9c24ba70899e3631791
Daniel Parron <danielparron.fernandes@gmail.com>
2025-04-10 16:08:29 -0300

    aumentou max size application

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { urlencoded, json } from 'express';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(json({ limit: '50mb' }));
+  app.use(urlencoded({ extended: true, limit: '50mb' }));
 
   // Configuração do Swagger
   const config = new DocumentBuilder()
