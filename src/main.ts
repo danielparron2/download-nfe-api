75f3197f68f89c924afbd027454fb229ff46648c
Daniel Parron <danielparron.fernandes@gmail.com>
2025-07-03 09:52:39 -0300

    cors google cloud

--- src/main.ts
+++ src/main.ts
@@ -29,7 +29,7 @@ async function bootstrap() {
   // Habilita o CORS para o domínio específico
   app.enableCors({
     //origin: 'http://localhost:3001',
-    origin: '*', 
+    origin: 'http://35.224.55.94', 
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     allowedHeaders: 'Content-Type, Accept, Authorization',
   });
